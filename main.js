82222c15469eb1d9ab8eefafab225b8289f23293
Stefany <ste_giovanetti@yahoo.it>
2025-02-08 11:51:56 +0100

    Esercizio Condizioni 1 eseguito con lo switch

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 let v = 29;
 
-if (v < 18) {
-    console.log('insufficiente');
-} else if (v >= 18 && v < 21) {
-    console.log('sufficiente');
-} else if (v >= 21 && v < 24) {
-    console.log('buono');
-} else if (v >= 24 && v < 27) {
-    console.log('distinto');
-} else if (v >= 27 && v <= 29) {
-    console.log('ottimo');
-} else if (v == 30) {
-    console.log('eccellente');
-} else {
-    console.log('voto non valido');  
+switch (true) {
+    case v < 18:
+        console.log('insufficiente');
+        break;
+
+    case v >= 18 && v < 21:
+        console.log('sufficiente');
+        break;
+
+    case v >= 21 && v < 24:
+        console.log('buono');
+        break;
+
+    case v >= 24 && v < 27:
+        console.log('distinto');
+        break;
+
+    case v >= 27 && v <= 29:
+        console.log('ottimo');
+        break;
+
+    case v == 30:
+        console.log('eccellente');
+        break;    
+
+    default:
+        console.log('voto non valido');
+        break;
 }
