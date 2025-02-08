9742abbdb787c99d23fd027dcd7faa00949f78f9
Stefany <ste_giovanetti@yahoo.it>
2025-02-08 11:36:48 +0100

    Esercizio Condizioni 1 eseguito con if/else

---
+++ main.js
@@ -0,0 +1,17 @@
+let v = 29;
+
+if (v < 18) {
+    console.log('insufficiente');
+} else if (v >= 18 && v < 21) {
+    console.log('sufficiente');
+} else if (v >= 21 && v < 24) {
+    console.log('buono');
+} else if (v >= 24 && v < 27) {
+    console.log('distinto');
+} else if (v >= 27 && v <= 29) {
+    console.log('ottimo');
+} else if (v == 30) {
+    console.log('eccellente');
+} else {
+    console.log('voto non valido');  
+}
